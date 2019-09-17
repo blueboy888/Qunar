6d9c63f83c3722f12e818c810a61a3edf5871dfe
许智海 <18829040039@163.com>
2019-09-17 20:14:04 +0800

    ...

--- hotel_list.py
+++ hotel_list.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # @Time    : 2019/9/17 18:30
-# @Author  : xuzhihai0723
+# @Author  : Esbiya
 # @Email   : 18829040039@163.com
 # @File    : hotel_list.py
 # @Software: PyCharm
--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # @Time    : 2019/9/17 18:31
-# @Author  : xuzhihai0723
+# @Author  : Esbiya
 # @Email   : 18829040039@163.com
 # @File    : settings.py
 # @Software: PyCharm
